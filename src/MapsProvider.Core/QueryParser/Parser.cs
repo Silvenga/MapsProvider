2b526843de3502cabfdf5f33d77dce397445d370
Mark Lopez <m@silvenga.com>
2017-06-16 22:43:56 -0500

    Completed basic parser prototype for bingmaps.

--- src/MapsProvider.Core/QueryParser/Parser.cs
+++ src/MapsProvider.Core/QueryParser/Parser.cs
@@ -1,6 +1,42 @@
-﻿namespace MapsProvider.Core.QueryParser
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+using MapsProvider.Core.QueryParser.Parsers;
+
+namespace MapsProvider.Core.QueryParser
 {
-    class Parser
+    public class Parser
     {
+        private static readonly Regex Regex = new Regex(@"bingmaps:\?(?<parm>[^&]+)(&(?<parm>[^&]+))*", RegexOptions.Compiled);
+
+        private static readonly IParameterParser<IParameter>[] ParameterParsers =
+        {
+            new CenterPoint(),
+            new BoundingBox(),
+            new Location(),
+            new QueryTerm(),
+            new ZoomLevel(),
+            new Traffic()
+        };
+
+        public IList<IParameter> Parse(string queryString)
+        {
+            var results = from param in ParseQueryGroups(queryString)
+                          from parser in ParameterParsers
+                          where parser.CanParse(param)
+                          select parser.Parse(param);
+
+            return results.ToList();
+        }
+
+        private IEnumerable<string> ParseQueryGroups(string queryString)
+        {
+            var match = Regex.Match(queryString);
+            foreach (Capture capture in match.Groups["parm"].Captures)
+            {
+                yield return capture.Value;
+            }
+        }
     }
 }
---
+++ tests/MapsProvider.Core.Tests/ParserFacts.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+
+using MapsProvider.Core.QueryParser;
+using MapsProvider.Core.QueryParser.Parsers;
+
+using Xunit;
+
+namespace MapsProvider.Core.Tests
+{
+    public class ParserFacts
+    {
+        [Fact]
+        public void Can_parse_single_param()
+        {
+            var parser = new Parser();
+
+            // Act
+            var results = parser.Parse("bingmaps:?cp=40.726966~-74.006076");
+
+            // Assert
+            results.Should().AllBeOfType<CenterPoint.Parameter>().And.ContainSingle();
+        }
+
+        [Fact]
+        public void Can_parse_multiple_params()
+        {
+            var parser = new Parser();
+
+            // Act
+            var results = parser.Parse("bingmaps:?cp=40.726966~-74.006076&bb=39.719_-74.52~41.71_-73.5");
+
+            // Assert
+            results.Should().HaveCount(2);
+        }
+    }
+}
